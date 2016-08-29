7c1d87c3111be4ad6b6bcbc0f3200224a355bd72
Mauricio Poppe <mauricio.poppe@gmail.com>
2016-08-29 17:55:45 -0400

    Ruby dotfiles maintenance

--- ruby/install.zsh
+++ ruby/install.zsh
@@ -13,27 +13,15 @@ main() {
     print-message "rvm already installed"
   fi
 
-  # activate nvm for the current subprocess
+  # activate rvm for the current subprocess
   [[ -s ${HOME}/.rvm/scripts/rvm ]] && . ${HOME}/.rvm/scripts/rvm
+
+  # link default gemset
+  ln -vfs ${DOTFILES_DIRECTORY}/ruby/default.gems ~/.rvm/gemsets/default.gems
 
   print-step "installing ruby 2.1.1"
   rvm install 2.1.1
-
-  # NOTE: first time I got http://stackoverflow.com/questions/10585002/zsh-complains-about-rvm-rvm-cleanse-variables-function-definition-file-not-fo
   rvm use ruby-2.1.1
-
-  # essential gems
-  print-step "installing gems..."
-
-  local gems
-  gems=(
-    rake
-    jekyll
-    travis
-  )
-  for gem in $gems; do
-    gem install $gem
-  done
 }
 
 main $@
