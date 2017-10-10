b29f93b858b09dc434c0b6cba113999a67d9bcf1
Mauricio Poppe <mauricio.poppe@gmail.com>
2017-10-10 13:13:54 -0700

    feat(zsh,homebrew): fzf installation moved to homebrew

--- zsh/init.source.zsh
+++ zsh/init.source.zsh
@@ -1,10 +1,3 @@
-# fzf (https://github.com/junegunn/fzf)
-#
-# The following line is commented because fzf checks with the following line
-# on install
-# __safe_source ${HOME}/.fzf.zsh
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
 # fasd (https://github.com/clvv/fasd)
 fasd_cache="${HOME}/.fasd-init-cache"
 if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
