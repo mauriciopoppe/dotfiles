7a7bd6be55960ccd4e1a561c33a257c0237ede7c
Mauricio Poppe <mauricio.poppe@gmail.com>
2019-01-17 19:56:54 -0800

    chore: iterm2 plist ignored

--- @macos/iTerm2/install.zsh
+++ @macos/iTerm2/install.zsh
@@ -1,7 +1,17 @@
 #!/bin/zsh
 
-[ "$(uname -s)" != "Darwin" ] && exit 0
+echo "Installing iTerm2"
 
-defaults write com.googlecode.iterm2 "PrefsCustomFolder" -string "$DOTFILES_DIRECTORY/@macos/iTerm2"
+ROOT=$DOTFILES_DIRECTORY/@macos/iTerm2
+CONFIG_DIR="$ROOT/config"
+defaults write com.googlecode.iterm2 "PrefsCustomFolder" -string $CONFIG_DIR
 defaults write com.googlecode.iterm2 "LoadPrefsFromCustomFolder" -bool true
 
+# copy plist file in config dir if it doesn't exist
+if [[ ! -f $CONFIG_DIR ]]; then
+  echo "creating iTerm config directory"
+  mkdir -p $CONFIG_DIR
+
+  echo "copying default configuration file"
+  cp $ROOT/com.googlecode.iterm2.plist $CONFIG_DIR
+fi
