a0aea30dbb8fcff32d8b95a455c508667c00bdad
Mauricio Poppe <mauriciopoppe@google.com>
2021-01-29 21:15:17 +0000

    tmux prefix plugin

--- go/auto.source.zsh
+++ go/auto.source.zsh
@@ -3,4 +3,4 @@
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 
-if [ -f "$HOME/.gvm/scripts/gvm" ]; then . "$HOME/.gvm/scripts/gvm"; fi
+# if [ -f "$HOME/.gvm/scripts/gvm" ]; then . "$HOME/.gvm/scripts/gvm"; fi
