5987df1970036c58cdd23a9c8121085e0fb399ec
Mauricio Poppe <mauricio.poppe@gmail.com>
2025-06-20 00:04:11 -0400

    zellij theme override with symlinks

--- zellij/install.zsh
+++ zellij/install.zsh
@@ -6,9 +6,11 @@ main() {
   print-header "zellij"
 
   mkdir -p ~/.config/zellij/plugins
+  mkdir -p ~/.config/zellij/themes
   symlink "${base}/config.kdl" "${HOME}/.config/zellij/config.kdl"
   symlink "${base}/layouts" "${HOME}/.config/zellij/layouts"
-  symlink "${base}/themes" "${HOME}/.config/zellij/themes"
+  symlink "${base}/themes/my-theme-dark" "${HOME}/.config/zellij/themes/my-theme-dark"
+  symlink "${base}/themes/my-theme-light" "${HOME}/.config/zellij/themes/my-theme-light"
   curl -LsSf https://github.com/fresh2dev/zellij-autolock/releases/download/0.2.2/zellij-autolock.wasm > ~/.config/zellij/plugins/zellij-autolock.wasm
 }
 
